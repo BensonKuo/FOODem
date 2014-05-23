e70dd108215778e9b1e615b438610f9562b7af22
HannahWang <b00901023@ntu.edu.tw>
2014-05-23 14:37:25 +0800

    aboutFB

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -11,13 +11,11 @@ function statusChangeCallback(response) {
         testAPI();
     } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
-        document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
+        document.getElementById('status').innerHTML = 'Please log into this app.';
     } else {
         // The person is not logged into Facebook, so we're not sure if
         // they are logged into this app or not.
-        document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
+        document.getElementById('status').innerHTML = 'Please log into Facebook.';
     }
 }
 
@@ -73,7 +71,7 @@ function checkfbButton(){
  var js, fjs = d.getElementsByTagName(s)[0];
  if (d.getElementById(id)) return;
  js = d.createElement(s); js.id = id;
- js.src = "//connect.facebook.net/en_US/sdk.js";
+ js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=635285726564421&version=v2.0";
  fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'));
 
