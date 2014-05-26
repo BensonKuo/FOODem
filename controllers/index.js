9539fd81dc28ae8fa56a76a6d284149c07e0d4b5
BensonKuo <bensonkuo17@gmail.com>
2014-05-26 23:27:08 +0800

    aaa

--- controllers/index.js
+++ controllers/index.js
@@ -4,3 +4,18 @@ var fs = require('fs');
 module.exports.index = function (req, res) {
   res.render('index', {title: 'Item Registration'});
 }
+
+
+
+
+module.exports.createitem = function (req, res) {
+  Item.create(req.body, function (Item) {
+    res.json(Item);
+  });
+}
+
+module.exports.load = function (req, res) {
+  Item.load(req.body, function (Item) {
+    res.json(Item);
+  });
+}
