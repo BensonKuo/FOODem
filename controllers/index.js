fe83094601d13a8253f1a39e6d355b2f48265b1c
BensonKuo <bensonkuo17@gmail.com>
2014-05-28 01:04:58 +0800

    kk

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,6 @@ module.exports.index = function (req, res) {
   res.render('index', {title: 'Item Registration'});
 }
 
-<<<<<<< HEAD
-
 
 
 module.exports.createitem = function (req, res) {
@@ -20,10 +18,10 @@ module.exports.load = function (req, res) {
     res.json(Item);
   });
 }
-=======
+
 module.exports.createAccount = function (req, res){
 	Account.create(req.body, function (Account) {
 	    res.json(Account);
 	});
 }
->>>>>>> 3b84a02b3e13dd86b3ef08f1caebfd59dfba9542
+
