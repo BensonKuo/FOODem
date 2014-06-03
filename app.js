89befd976cc3d03fa2e73b24b33fe44b06883fca
HannahWang <b00901023@ntu.edu.tw>
2014-06-04 01:16:42 +0800

    delete

---
+++ app.js
@@ -0,0 +1,29 @@
+//mongoose setup
+require('./db');
+
+var express = require('express');
+var routes = require('./routes');
+var food = require('./routes/food.js'); //不知道要不要改?
+var http = require('http');
+var path = require('path');
+
+
+//指定收到get or post的時候 會執行某一個function
+var app = express();
+
+app.get('/', routes.index);
+
+//get all food
+app.get('/foods', food.list );
+
+//create a food
+app.post('/foods',food.create);
+
+//show a food
+app.get('/foods/:id', food.show);
+
+//update a food
+app.post('/foods/:id', food.update);
+
+//delete a food
+app.delete('/foods/:id', food.destroy);
