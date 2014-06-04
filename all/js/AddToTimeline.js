d1e1aa4190b32bc65ba5833cd9c853d265815807
BensonKuo <bensonkuo17@gmail.com>
2014-06-04 15:06:11 +0800

    fff

--- all/js/AddToTimeline.js
+++ all/js/AddToTimeline.js
@@ -2,11 +2,22 @@
 (function(){
 	
 
+var tmplh='<h2 class="post-title"></h2>',
+	tmplb='<p class="post-meta"></p>',
+	mybutton = $('.mybutton');
+
+	//test only
+	mybutton.click(function(){
+		mybutton.addclass('post-meta')
+		
+		console.log("test success~");
+	});
 
 
 
 
-	
+
+
 })
 
 
